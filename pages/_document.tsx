822d9a247932e9489b0440fc3bc63cd0fbf11ff6
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-07-19 15:16:04 +0700

    refactor: simplify Head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
